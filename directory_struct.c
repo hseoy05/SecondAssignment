c7229c8bab4c3c8528748c6627f265e505fbd06c
SeoYeon <tjdus121234@korea.ac.kr>
2025-04-15 15:39:34 +0900

    fork()함수 직접 추가 후 리눅스 쉘에서 빌드함.
    문제점: cd 구현이 제대로 되지 않음.

--- directory_struct.c
+++ directory_struct.c
@@ -10,14 +10,14 @@ Node* root = NULL;
 
 void directoryStart() {
 	root = malloc(sizeof(Node));
-	strcpy_s(root->name, sizeof(root->name), "/");
+	strcpy(root->name, "/");
 	root->parent = NULL;
 	root->child = NULL;
 	root->sibling = NULL;
 	root->file = NULL;
 
 	Node* bin = malloc(sizeof(Node));
-	strcpy_s(bin->name, sizeof(bin->name), "bin");
+	strcpy(bin->name, "bin");
 	bin->parent = root;
 	bin->child = NULL;
 	bin->sibling = NULL;
@@ -25,21 +25,21 @@ void directoryStart() {
 	fileStart(bin);
 
 	Node* user = malloc(sizeof(Node));
-	strcpy_s(user->name, sizeof(user->name), "user");
+	strcpy(user->name, "user");
 	user->parent = root;
 	user->child = NULL;
 	user->sibling = bin;
 	user->file = NULL;
 
 	Node* ect = malloc(sizeof(Node));
-	strcpy_s(ect->name, sizeof(ect->name), "ect");
+	strcpy(ect->name, "ect");
 	ect->parent = root;
 	ect->child = NULL;
 	ect->sibling = user;
 	ect->file = NULL;
 
 	Node* home = malloc(sizeof(Node));
-	strcpy_s(home->name, sizeof(home->name), "home");
+	strcpy(home->name, "home");
 	home->parent = root;
 	home->child = NULL;
 	home->sibling = ect;
@@ -48,7 +48,7 @@ void directoryStart() {
 	root->child = home;
 
 	Node* user1 = malloc(sizeof(Node));
-	strcpy_s(user1->name, sizeof(user1->name), "oepickle");
+	strcpy(user1->name,"oepickle");
 	user1->parent = home;
 	user1->child = NULL;
 	user1->sibling = NULL;
@@ -59,6 +59,6 @@ void directoryStart() {
 
 void fileStart(Node* connectNode){
 	File* lib = malloc(sizeof(File));
-	strcpy_s(lib->name, sizeof(lib->name), "file1");
+	strcpy(lib->name, "file1");
 	connectNode->file = lib;
 }
